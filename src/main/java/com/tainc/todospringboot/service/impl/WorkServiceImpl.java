3ca155e2697716cf9ac932bbd0437a462a192b9c
Nguyen Cong Tai <nguyen.cong.tai@framgia.com>
2019-05-18 09:52:01 +0700

    [task][implement_CRUD] implement paging and sorting api

--- src/main/java/com/tainc/todospringboot/service/impl/WorkServiceImpl.java
+++ src/main/java/com/tainc/todospringboot/service/impl/WorkServiceImpl.java
@@ -1,9 +1,23 @@
 package com.tainc.todospringboot.service.impl;
 
+import com.tainc.todospringboot.model.WorkEntity;
+import com.tainc.todospringboot.repository.WorkRepository;
 import com.tainc.todospringboot.service.WorkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorkServiceImpl implements WorkService {
+
+    @Autowired
+    private WorkRepository workRepository;
+
+    @Override
+    public Page<WorkEntity> findAll(Pageable page) {
+        return workRepository.findAll(page);
+    }
+
 
 }
