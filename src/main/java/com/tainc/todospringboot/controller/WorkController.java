03edcb3a7b45e1c24c8ab9a65b6f138783a9fdbe
unknown <congtaiminsk@gmail.com>
2020-04-15 22:55:32 +0700

    init dev

--- src/main/java/com/tainc/todospringboot/controller/WorkController.java
+++ src/main/java/com/tainc/todospringboot/controller/WorkController.java
@@ -30,112 +30,11 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/works")
 public class WorkController {
-
-    @Autowired
-    WorkService workService;
-
-    /**
-     * get works by paging and sorting
-     * @param sortBy sort works by field
-     * @param order order it
-     * @param page current page
-     * @param pageSize number of records in page
-     * @return page entity if OK, else NO_CONTENT
-     */
-    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Page<WorkEntity>> findAll(@RequestParam("sortBy") String sortBy,
-                                                    @RequestParam("order") String order,
-                                                    @RequestParam("page") int page,
-                                                    @RequestParam("pageSize") int pageSize) {
-        Sort sortType = Sort.by(sortBy);
-        Pageable pageableWithSort = PageRequest
-            .of(page, pageSize,
-                order.equals("asc") ? sortType.ascending() : sortType.descending());
-        Page<WorkEntity> workPage = workService.findAll(pageableWithSort);
-        if (workPage.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(workPage, HttpStatus.OK);
-    }
-
-    /**
-     * get work by id
-     * @param id id of work
-     * @return work and status
-     */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = {
-        MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<WorkEntity> getById(
-        @PathVariable("id") Integer id) {
-        Optional<WorkEntity> work = workService.findById(id);
-        if (!work.isPresent()) {
-            throw new WorkNotFoundException("Work not found: id = " + id);
-        }
-        return new ResponseEntity<>(work.get(), HttpStatus.OK);
-    }
-
-    /**
-     * create new work
-     * @param workForm form to create
-     * @return new work response if success
-     */
-    @RequestMapping(method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<WorkEntity> create(@RequestBody @Valid WorkForm workForm) {
-        WorkEntity workEntity = new WorkEntity();
-        try {
-            BeanUtils.copyProperties(workForm, workEntity);
-            workEntity = workService.save(workEntity);
-        } catch (DataAccessException ex) {
-            throw new WorkExistException("This work is already exist");
-        }
-        return new ResponseEntity<>(workEntity, HttpStatus.CREATED);
-    }
-
-    /**
-     * update work
-     * @param id id of work
-     * @param workForm form to update
-     * @return updated work if success
-     */
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = {
-        MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<WorkEntity> update(@PathVariable("id") Integer id,
-                                             @RequestBody @Valid WorkForm workForm) {
-
-        Optional<WorkEntity> currentWork = workService.findById(id);
-
-        if (!currentWork.isPresent()) {
-            throw new WorkNotFoundException("Invalid work id : " + id);
-        }
-
-        BeanUtils.copyProperties(workForm, currentWork.get());
-        try {
-            workService.save(currentWork.get());
-        } catch (DataAccessException ex) {
-            throw new WorkExistException("This work is already exist");
-        }
-
-        return new ResponseEntity<>(currentWork.get(), HttpStatus.OK);
-    }
-
-    /**
-     * destroy work
-     * @param id work's id
-     * @return message and status response
-     */
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = {
-        MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Messages> delete(@PathVariable("id") Integer id) {
-        try {
-            workService.remove(id);
-        } catch (EmptyResultDataAccessException ex) {
-            throw new WorkNotFoundException("Invalid work id : " + id);
-        }
-        return new ResponseEntity<>(new Messages("Delete Work Successfull", true), HttpStatus.OK);
-    }
+    // function 1
+    // function 2
+    // function 3
+    // function 4
+    // function 5
+    // function 6
+    // function 7
 }
